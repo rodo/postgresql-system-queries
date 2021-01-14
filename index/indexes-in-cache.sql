04d8e01ce47b496d19e6bedb55d721e3ebdb9b5f
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2021-01-14 16:54:39 +0100

    FIX index relname

--- index/indexes-in-cache.sql
+++ index/indexes-in-cache.sql
@@ -1,8 +1,10 @@
--- how many indexes are in cache
-SELECT sum(idx_blks_read) AS idx_read,
-       sum(idx_blks_hit)  AS idx_hit,
-       CASE WHEN sum(idx_blks_hit) = 0
-       THEN 0 ELSE (sum(idx_blks_hit) - sum(idx_blks_read)) / sum(idx_blks_hit)
-       END as ratio
-       
-FROM pg_statio_user_indexes;
+-- how many indexes usage ratio
+  SELECT indexrelname,
+         sum(idx_blks_read) AS blks_read,
+         sum(idx_blks_hit)  AS blks_hit,
+         CASE WHEN sum(idx_blks_hit) = 0
+         THEN 0 ELSE (sum(idx_blks_hit) - sum(idx_blks_read)) / sum(idx_blks_hit)
+         END as ratio       
+    FROM pg_statio_user_indexes    
+GROUP BY indexrelname
+ORDER BY indexrelname DESC;
