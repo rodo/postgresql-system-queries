0d173f638225404436d9fb7432b50c720c6a5878
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2023-11-08 09:31:19 +0100

    Add two new queries about locks

--- locks-stats.sql
+++ locks-stats.sql
@@ -2,16 +2,14 @@
 SELECT pg_database.datname,
        pg_class.relname,
        pg_locks.locktype,
-       pg_locks.page,
-       pg_locks.tuple,
-       pg_locks.virtualxid,
-       pg_locks.transactionid,
-       pg_locks.classid,
-       pg_locks.pid,
        pg_locks.mode,
-       pg_locks.granted
-
+       pg_locks.granted,
+       count(*)
 FROM pg_locks
 
 JOIN pg_class ON pg_locks.relation = pg_class.oid
 JOIN pg_database ON pg_locks.database = pg_database.oid
+
+GROUP BY pg_database.datname,pg_class.relname,       pg_locks.locktype,
+       pg_locks.mode,
+       pg_locks.granted
--- table/table-size.sql
+++ table/table-size.sql
@@ -1,4 +1,20 @@
 -- table index usage rates (should not be less than 0.99)
-SELECT relname AS table , CASE WHEN (seq_scan + idx_scan) = 0 THEN 0 ELSE (100 * idx_scan / (seq_scan + idx_scan)) END as percent_of_times_index_used, n_live_tup rows_in_table
-FROM pg_stat_user_tables 
-ORDER BY n_live_tup DESC;
+SELECT table_schema,table_name, row_estimate
+       , pg_size_pretty(total_bytes) AS total
+       , pg_size_pretty(table_bytes) AS table       
+       , pg_size_pretty(index_bytes) AS index
+       , pg_size_pretty(toast_bytes) AS toast
+
+  FROM (
+  SELECT *, total_bytes-index_bytes-coalesce(toast_bytes,0) AS table_bytes FROM (
+      SELECT c.oid,nspname AS table_schema, relname AS table_name
+              , c.reltuples AS row_estimate
+              , pg_total_relation_size(c.oid) AS total_bytes
+              , pg_indexes_size(c.oid) AS index_bytes
+              , pg_total_relation_size(reltoastrelid) AS toast_bytes
+          FROM pg_class c
+          LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
+          WHERE relkind = 'r' AND nspname NOT IN ('pg_catalog','information_schema')
+          ORDER BY total_bytes DESC, relname
+  ) a
+) a;
